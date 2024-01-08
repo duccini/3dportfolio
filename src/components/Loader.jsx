9641fc15c4a3333f3ea2ba36d8a2966e694d2afb
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-01-07 22:42:12 -0300

    load hero 3d model

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+  const { progress } = useProgress();
 
-export default Loader
+  return (
+    <Html>
+      <span className="canvas-loader"></span>
+      <p
+        style={{
+          fontSize: 20,
+          color: "#f1f1f1",
+          fontWeight: 400,
+          marginTop: 40,
+          textAlign: "center",
+        }}
+      >
+        Loading {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default Loader;
